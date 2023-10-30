e802d6aa5b3717b89d8edbd127ca89a33bc87eee
AdityaGavali <adityagavali18@gmail.com>
2023-10-30 19:56:37 +0530

    changes

---
+++ order.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+
+class MealItem:
+    def __init__(self, id, quantity, menu_item):
+        self.__meal_item_id = id
+        self.__quantity = quantity
+        self.__menu_item = menu_item
+
+    def update_quantity(self, quantity):
+        self.__quantity = quantity
+
+
+class Meal:
+    def __init__(self, id, seat):
+        self.__meal_id = id
+        self.__seat = seat
+        self.__menu_items = []
+
+    def add_meal_item(self, meal_item):
+        self.__menu_items.append(meal_item)
+
+
+class Check():
+    def __init__(self):
+        None
+
+
+class Order:
+    def __init__(self, id, status, table, waiter, chef):
+        self.__order_id = id
+        self.__OrderStatus = status
+        self.__creation_time = datetime.now()
+
+        self.__meals = []
+        self.__table = table
+        self.__waiter = waiter
+        self.__chef = chef
+        self.__check = Check()
+
+    def add_meal(self, meal):
+        self.__meals.append(meal)
+
+    def remove_meal(self, meal):
+        self.__meals.remove(meal)
+
+    def get_status(self):
+        return self.__OrderStatus
+
+    def set_status(self, status):
+        self.__OrderStatus = status
+
---
+++ people.py
@@ -0,0 +1,58 @@
+from abc import ABC
+from datetime import datetime
+from constants import *
+
+
+
+class Account:
+    def __init__(self, id, password, address, status=AccountStatus.Active):
+        self.__id = id
+        self.__password = password
+        self.__address = address
+        self.__status = status
+
+    def reset_password(self):
+        None
+
+
+class Person(ABC):
+    def __init__(self, name, email, phone):
+        self.__name = name
+        self.__email = email
+        self.__phone = phone
+
+
+class Employee(ABC):
+    def __init__(self, id, account, name, email, phone):
+        super().__init__(name, email, phone)
+        self.__employee_id = id
+        self.__date_joined = datetime.date.today()
+        self.__account = account
+
+
+class Receptionist(Employee):
+    def __init__(self, id, account, name, email, phone):
+        super().__init__(id, account, name, email, phone)
+
+    def create_reservation(self):
+        None
+
+    def search_customer(self, name):
+        None
+
+
+class Manager(Employee):
+    def __init__(self, id, account, name, email, phone):
+        super().__init__(id, account, name, email, phone)
+
+    def add_employee(self):
+        None
+
+
+class Chef(Employee):
+    def __init__(self, id, account, name, email, phone):
+        super().__init__(id, account, name, email, phone)
+
+    def take_order(self):
+        None
+
