f75994a2e513e81af4b81fb6fcc8802441d6af80
AdityaGavali <adityagavali18@gmail.com>
2023-10-31 07:12:30 +0530

    curses library added

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ from order import MealItem, Meal, Check, Order
 from people import Account, Receptionist, Manager, Chef
 
 
-kitchen = Kitchen("Main Kitchen")
-branch = Branch("Branch 1", "123 Main St", kitchen)
-restaurant = Restaurant("My Restaurant")
-restaurant.add_branch(branch)
+# kitchen = Kitchen("Main Kitchen")
+# branch = Branch("Branch 1", "123 Main St", kitchen)
+# restaurant = Restaurant("My Restaurant")
+# restaurant.add_branch(branch)
 
-menu = Menu(1, "Main Menu", "Delicious dishes")
-section1 = MenuSection(1, "Starters", "Appetizers to start your meal")
-section1.add_menu_item(MenuItem(1, "Salad", "Healthy salad with greens", 5.99))
-section1.add_menu_item(MenuItem(2, "Soup", "Soup of the day", 4.99))
-menu.add_menu_section(section1)
+# menu = Menu(1, "Main Menu", "Delicious dishes")
+# section1 = MenuSection(1, "Starters", "Appetizers to start your meal")
+# section1.add_menu_item(MenuItem(1, "Salad", "Healthy salad with greens", 5.99))
+# section1.add_menu_item(MenuItem(2, "Soup", "Soup of the day", 4.99))
+# menu.add_menu_section(section1)
 
-table1 = Table(1, 4, "A1")
-table2 = Table(2, 6, "B1")
+# table1 = Table(1, 4, "A1")
+# table2 = Table(2, 6, "B1")
 
     # Creating Orders and Meals
 # meal_item1 = MealItem(1, 2, menu.menu_sections[0].menu_items[0])
@@ -28,23 +28,21 @@ table2 = Table(2, 6, "B1")
 # meal1.add_meal_item(meal_item2)
 
     # Creating Reservation and Assigning Table
-customer_account = Account(1, "password123", "123 Customer St")
-reservation = Reservation(1, 4, "Special Occasion", customer_account)
-reservation.add_table(table1)
+# customer_account = Account(1, "password123", "123 Customer St")
+# reservation = Reservation(1, 4, "Special Occasion", customer_account)
+# reservation.add_table(table1)
 
     # Creating Employees
-receptionist_account = Account(2, "receptionist123", "123 Reception St")
-receptionist = Receptionist(1, receptionist_account, "Receptionist Name", "receptionist@example.com", "123-456-7890")
+# receptionist_account = Account(2, "receptionist123", "123 Reception St")
+# receptionist = Receptionist(1, receptionist_account, "Receptionist Name", "receptionist@example.com", "123-456-7890")
 
-manager_account = Account(3, "manager123", "123 Manager St")
-manager = Manager(2, manager_account, "Manager Name", "manager@example.com", "123-456-7890")
+# manager_account = Account(3, "manager123", "123 Manager St")
+# manager = Manager(2, manager_account, "Manager Name", "manager@example.com", "123-456-7890")
 
-chef_account = Account(4, "chef123", "123 Chef St")
-chef = Chef(3, chef_account, "Chef Name", "chef@example.com", "123-456-7890")
+# chef_account = Account(4, "chef123", "123 Chef St")
+# chef = Chef(3, chef_account, "Chef Name", "chef@example.com", "123-456-7890")
 
     # Creating Orders and Adding Meals
-order = Order(1, OrderStatus.RECEIVED, table1, receptionist, chef)
-order.add_meal(meal1)
 
     # Printing Reservation Details (same as before, no changes here)
 
@@ -55,9 +53,42 @@ print("\nBefore Menu Item Modification:")
 print("Menu Items in Section 1:")
 
 
-
+print("Hello")
 
 print("\nAfter Menu Item Modification:")
 print("Menu Items in Section 1:")
 
 
+import curses
+
+def main(stdscr):
+    curses.curs_set(0)  # Hide the cursor
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Welcome to our Restaurant Management System !!", curses.A_BOLD)
+    stdscr.addstr(2, 0, "1. View Menu")
+    stdscr.addstr(3, 0, "2. Place Order")
+    stdscr.addstr(4, 0, "3. View Bill")
+    stdscr.addstr(5, 0, "4. Exit")
+    stdscr.refresh()
+
+    while True:
+        choice = stdscr.getch()
+        stdscr.clear()
+
+        if choice == ord('1'):
+            # Code for viewing menu goes here
+            stdscr.addstr(0, 0, "View Menu Option Selected", curses.A_BOLD)
+        elif choice == ord('2'):
+            # Code for placing an order goes here
+            stdscr.addstr(0, 0, "Place Order Option Selected", curses.A_BOLD)
+        elif choice == ord('3'):
+            # Code for viewing orders goes here
+            stdscr.addstr(0, 0, "View Bill Option Selected", curses.A_BOLD)
+        elif choice == ord('4'):
+            break
+        else:
+            stdscr.addstr(0, 0, "Invalid Choice. Please try again.", curses.A_BOLD)
+
+        stdscr.refresh()
+
+curses.wrapper(main)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+
+# kitchen = Kitchen("Main Kitchen")
+# branch = Branch("Branch 1", "123 Main St", kitchen)
+# restaurant = Restaurant("My Restaurant")
+# restaurant.add_branch(branch)
+
+# menu = Menu(1, "Main Menu", "Delicious dishes")
+# section1 = MenuSection(1, "Starters", "Appetizers to start your meal")
+# section1.add_menu_item(MenuItem(1, "Salad", "Healthy salad with greens", 5.99))
+# section1.add_menu_item(MenuItem(2, "Soup", "Soup of the day", 4.99))
+# menu.add_menu_section(section1)
+
+# table1 = Table(1, 4, "A1")
+# table2 = Table(2, 6, "B1")
